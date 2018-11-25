9f78f42d790f092e124e0b4b2b39fdcf8c51520a
hmalik88 <hbmalik88@gmail.com>
2018-11-25 18:52:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n) {
+  const uls = document.querySelectorAll('ul.ranked-list');
+ for (let i = 0; i < uls.length; i ++) {
+  children = uls[i].children
+  for (let f = 0; f < children.length; f++) {
+    children[f].innerHTML = parseInt(children[f].innerHTML) + n;
+  }
+ }
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
